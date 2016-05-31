d0895105a079fe6a96db5bc06d6075981d4dd77a
art-Hasan <guseyin.gasymov@mail.ru>
2016-05-31 21:21:20 +0300

    learned models, templates and views

--- src/rango/admin.py
+++ src/rango/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
-class PageInline(admin.StackedInline):
-    model = Page
-    extra = 3
-
+# class PageInline(admin.StackedInline):
+#     model = Page
+#     extra = 0
+#
 class CategoryAdmin(admin.ModelAdmin):
-    fields = ['name']
-    inlines = [PageInline]
+    prepopulated_fields = {'slug': ('name',)}
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
 
 admin.site.register(Category, CategoryAdmin)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
