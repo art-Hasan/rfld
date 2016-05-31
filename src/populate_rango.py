2bc43b76ab15acf334544891d8912c387451cb4b
art-Hasan <guseyin.gasymov@mail.ru>
2016-05-31 19:53:05 +0300

    added populate script

--- src/populate_rango.py
+++ src/populate_rango.py
@@ -7,7 +7,7 @@ django.setup()
 from rango.models import Category, Page
 
 def populate():
-	python_cat = add_cat('Python')
+	python_cat = add_cat("Python", views=128, likes=64)
 
 	add_page(cat=python_cat, title="Official Python Tutorial", url="http://docs.python.org/2/tutorial/")
 
@@ -15,7 +15,7 @@ def populate():
 
 	add_page(cat=python_cat, title="Learn Python in 10 Minutes", url="http://www.korokithakis.net/tutorials/python/")
 
-	django_cat = add_cat("Django")
+	django_cat = add_cat("Django", views=64, likes=32)
 
 	add_page(cat=django_cat, title="Official Django Tutorial", url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/")
 
@@ -23,7 +23,7 @@ def populate():
 
 	add_page(cat=django_cat, title="How to Tango with Django", url="http://www.tangowithdjango.com/")
 
-	frame_cat = add_cat("Other Frameworks")
+	frame_cat = add_cat("Other Frameworks", views=32, likes=16)
 
 	add_page(cat=frame_cat, title="Bottle", url="http://bottlepy.org/docs/dev/")
 
@@ -41,8 +41,11 @@ def add_page(cat, title, url, views=0):
 	p.save()
 	return p
 
-def add_cat(name):
+def add_cat(name, views=0, likes=0):
 	c = Category.objects.get_or_create(name=name)[0]
+	c.views=views
+	c.likes=likes
+	c.save()
 	return c
 
 # Start execution here!
